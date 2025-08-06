a5472a132b2eac1c2378ad7d32279066d9ad15e0
Azizul Haque Aziz <khanazizul1199@gmail.com>
2025-08-06 16:28:21 +0600

    Organize files app

---
+++ projects/organize_files/organize_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p TXT PDF
+
+# move file based on extension
+#__Move .txt file
+for file in *.txt; do
+   [ -f "${file}" ] && mv "${file}" TXT/
+done
+
+#__Move .pdf file
+for file in *.pdf; do
+    [ -f "${file}" ] && mv "${file}" PDF/
+done
+
+
+
+echo "All file moved successfully"
